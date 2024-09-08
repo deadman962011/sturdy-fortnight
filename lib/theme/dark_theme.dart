c37d74d0bf5e2f547bca7b552a93d929133d016b
shamelapp2025 <shamelapp2025@gmail.com>
2024-09-08 19:48:37 +0400

    9/8

--- lib/theme/dark_theme.dart
+++ lib/theme/dark_theme.dart
@@ -3,15 +3,15 @@ import 'package:shamel_store_app/util/app_constants.dart';
 
 ThemeData dark = ThemeData(
   fontFamily: AppConstants.fontFamily,
-  primaryColor: const Color(0xFF54b46b),
+  primaryColor: const Color.fromRGBO(242, 157, 65, 1),
   secondaryHeaderColor: const Color(0xFF010d75),
   disabledColor: const Color(0xFF6f7275),
   brightness: Brightness.dark,
   hintColor: const Color(0xFFbebebe),
   cardColor: Colors.black,
   shadowColor: Colors.white.withOpacity(0.03),
-  textButtonTheme: TextButtonThemeData(style: TextButton.styleFrom(foregroundColor: const Color(0xFF54b46b))),
-  colorScheme: const ColorScheme.dark(primary: Color(0xFF54b46b), secondary: Color(0xFF54b46b)).copyWith(error: const Color(0xFFdd3135)),
+  textButtonTheme: TextButtonThemeData(style: TextButton.styleFrom(foregroundColor: const Color.fromARGB(255, 255, 196, 132))),
+  colorScheme: const ColorScheme.dark(primary: const Color.fromARGB(255, 255, 196, 132), secondary: const Color.fromARGB(255, 255, 196, 132)).copyWith(error: const Color(0xFFdd3135)),
   popupMenuTheme: const PopupMenuThemeData(color: Color(0xFF29292D), surfaceTintColor: Color(0xFF29292D)),
   dialogTheme: const DialogTheme(surfaceTintColor: Colors.white10),
   floatingActionButtonTheme: FloatingActionButtonThemeData(shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(500))),
